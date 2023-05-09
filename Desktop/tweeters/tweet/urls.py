cadbc5b2abce43ab4fe9ffa39e9b3f18a6007b06
james king <jamesasuelimen77@gmail.com>
2023-05-09 11:07:56 +0100

    update

--- Desktop/tweeters/tweet/urls.py
+++ Desktop/tweeters/tweet/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('welcome/', views.welcome, name="home")
+    path('welcome/', views.welcome, name="home"),
+    path('tweet_detail/<int: pk>/', views.tweet_details, name="tweet_details"),
 ]
--- Desktop/tweeters/tweeters/settings.py
+++ Desktop/tweeters/tweeters/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'tweeters.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tweeter_db',
+        'USER': 'root',
+        'PASSWORD': 'Kingdraw@77',
+        'HOST': 'localhost',
+        'PORT': 3306
     }
 }
 
