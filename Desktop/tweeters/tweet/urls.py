7d52b0de29fdaab9540593a07d6274aa746348e6
James Asuelimen <113517510+jamesking77-create@users.noreply.github.com>
2023-11-29 10:54:01 +0100

    Delete Desktop/tweeters directory

--- Desktop/tweeters/tweet/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('welcome/', views.welcome, name="home"),
-    path('tweet_detail/<int: pk>/', views.tweet_details, name="tweet_details"),
-]
